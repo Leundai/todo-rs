250a1b81c127c92750e0cda213343787fd718295
leundai <leogalindofrias@gmail.com>
2024-03-06 21:40:56 -0600

    Add clap and subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    debug: bool,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Add a new todo
+    Add {
+        name: String
+    },
+    /// Mark as done
+    Done {
+        name: String
+    },
+    /// Remove a todo
+    Remove {
+        name: String
+    },
+}
+
 fn main() {
     println!("Hello, world!");
+    let cli = Cli::parse();
 }
